921a36c495e4e906f1b254eddfbf425dbda46415
sonyaEgorova <sonegor_96@mail.ru>
2016-01-11 16:07:19 +0300

    NewClasses

---
+++ ConsoleApplication2/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar16
+{
+    class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
